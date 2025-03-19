d427601edb1a5e6bc5aede37185419d36033cac8
Brad Gessler <bradgessler@gmail.com>
2025-03-19 12:17:21 -0700

    Organize project into sub-commands

---
+++ app/terminal/main_terminal/authorization_commands.rb
@@ -0,0 +1,33 @@
+class MainTerminal::AuthorizationCommands < ApplicationTerminal
+  desc "login", "Login to your account"
+  def login
+    Terminalwire::Rails::Channel.new do |channel|
+      url = terminal_authorization_url(channel)
+      puts "Launching #{url}"
+      browser.launch url
+
+      case JSON.parse(channel.read.data, symbolize_names: true)
+      in status: "approved", user_id:
+        self.current_user = User.find(user_id)
+        puts "Logged in in as #{current_user.email}."
+      else
+        fail "Login failed."
+      end
+    end
+  end
+
+  desc "whoami", "Displays current user information."
+  def whoami
+    if self.current_user
+      puts "Logged in as #{current_user.email}."
+    else
+      fail "Not logged in. Run `#{self.class.basename} login` to login."
+    end
+  end
+
+  desc "logout", "Logout of your account"
+  def logout
+    session.reset
+    puts "Successfully logged out."
+  end
+end
