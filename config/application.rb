acda751954ed322ed8c337d6af49cfb169555208
Brad Gessler <bradgessler@gmail.com>
2025-03-19 15:06:30 -0700

    Use async-cable

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'async/cable'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
